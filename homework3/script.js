447ffe47439bb01229b69a60f78e424b73e51584
OlgaAk <binpolyglotte@gmail.com>
2017-01-08 21:15:22 +0300

    Fixed task 1 (days) and task 2 (sum digits)

--- homework3/script.js
+++ homework3/script.js
@@ -5,36 +5,33 @@
 
 console.log('Task 1');
 
- function getDay (number, one, two, three) {
-      
-    if (number >= 5 && number <= 20) {
-        return three;
-    } else if (number == 1) {
-        return one;
-    } else if (number >= 2 && number <= 4) {
-        return two;
-    }
-    return three;
-} 
-console.log(getDay(1,  'день', 'дня', 'дней'));
-console.log(getDay(22,  'день', 'дня', 'дней'));
-console.log(getDay(29,  'день', 'дня', 'дней'));
+ function getDay(n) {
+        if (n%10 == 1 && n != 11) {
+            console.log(n + ' день');
+        } else if ( (n%10 > 1 && n%10 < 5) && (n < 11 || n > 14) ) {
+            console.log(n + ' дня');
+        } else {
+             console.log(n + ' дней');
+        }
+    };
 
+    getDay(3);
+    getDay(21);
+    getDay(6);
 
 
 
 ///// Task 2
 
-console.log('Task 2');
-var x = 16;
-var sum = 0;
-while (x > 0) {
-    sum = sum + x%10; 
-    x = Math.floor(x/10);
-}
 
-console.log(sum);
-
+function sumDigit (x) {
+        if (x >= 10) {
+            return x%10 + sumDigit(Math.floor(x/10));
+        } else {
+            return  x;    
+        }         
+    };
+    console.log(sumDigit(953));
 
 
 ///// Task 3
