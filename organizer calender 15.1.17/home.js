88f799a38217f7261ec09f6a2b3262a8939603e7
OlgaAk <binpolyglotte@gmail.com>
2017-01-17 16:31:55 +0300

    Monday first

--- organizer calender 15.1.17/home.js
+++ organizer calender 15.1.17/home.js
@@ -23,17 +23,46 @@ var daysInCurrentMonth = daysInMonth(currentMonth, currentYear);
 console.log(daysInCurrentMonth);
 
 function createCalender (daysInCurrentMonth) {
+
 for (var i = 0; i < daysInCurrentMonth; i++) {
+
 var cDay = document.createElement('div');
 cDay.setAttribute('class', 'calenderDay');
 var dayNumP = document.createElement('p');
 var dayNumT = document.createTextNode(i+1);
+cDay.setAttribute('id', i+1);
+var cSpan = document.createElement('span');
+cSpan.setAttribute('class', 'dayPlans');
 dayNumP.appendChild(dayNumT);
 cDay.appendChild(dayNumP);
+cDay.appendChild(cSpan);
 calenderDiv.appendChild(cDay);
+}
+
+}
+
+
+function findMonday () {
+
+    for (var i = 0; i < 7; i++) {
+    var thatDate = new Date (currentYear, currentMonth, i);
+var thatDateofWeek = thatDate.getDay();
+console.log(thatDateofWeek);
+
+if (thatDateofWeek === 0) {
+    return;
+}
+var cDay = document.createElement('div');
+cDay.setAttribute('class', 'calenderDay');
+
+var dayNumP = document.createElement('p');
+cDay.appendChild(dayNumP);
+calenderDiv.appendChild(cDay);
+
 }
 }
 
+findMonday();
 createCalender(daysInCurrentMonth);
 
 } (window));
