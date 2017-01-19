df8dd0dad7e7d305bd1cdb7141b4dd99b901d951
OlgaAk <binpolyglotte@gmail.com>
2017-01-19 16:27:29 +0300

    Fixed + Lesson days selected with color

--- organizer calender 15.1.17/home.js
+++ organizer calender 15.1.17/home.js
@@ -12,9 +12,6 @@ function daysInMonth(month,year) {
     return new Date(year, month, 0).getDate();
 }
 
-// function findWeekday (currentMonth) {
-//    getWeekDay
-// }
 
 console.log(currentDate);
 
@@ -31,11 +28,12 @@ cDay.setAttribute('class', 'calenderDay');
 var dayNumP = document.createElement('p');
 var dayNumT = document.createTextNode(i+1);
 cDay.setAttribute('id', i+1);
-var cSpan = document.createElement('span');
-cSpan.setAttribute('class', 'dayPlans');
+dayNumP.setAttribute('id', "p" + (i+1));
+// var cSpan = document.createElement('span');
+// cSpan.setAttribute('class', 'dayPlans');
 dayNumP.appendChild(dayNumT);
 cDay.appendChild(dayNumP);
-cDay.appendChild(cSpan);
+// cDay.appendChild(cSpan);
 calenderDiv.appendChild(cDay);
 }
 
@@ -50,29 +48,58 @@ var thatDateofWeek = thatDate.getDay();
 console.log(thatDateofWeek);
 
 if (thatDateofWeek === 0) {
-    return;
-}
+    console.log(i);
+ for (var counter = 0; counter < (7-i); counter++) {
 var cDay = document.createElement('div');
 cDay.setAttribute('class', 'calenderDay');
 
 var dayNumP = document.createElement('p');
 cDay.appendChild(dayNumP);
 calenderDiv.appendChild(cDay);
+    
+}
 
 }
-}
+}}
 
 var form = document.getElementById('divForm');
 
 
-calenderDiv.addEventListener('click', function () { form.style.display = display-block; } );
+calenderDiv.addEventListener('click', function () { form.style.display = "block"; } );
+
+var selectedDays = [];
+var lessonDays = [2, 4, 6];
+var currentLesson = ["Морской бой",  "Node.js", "Тема уточняется"]
 
 
+
+function  getSelectedDays (lessonDays) {
+      for (var i = 1; i <= daysInCurrentMonth; i++) {
+    var thatDate = new Date (currentYear, currentMonth, i);
+        var thatDateofWeek = thatDate.getDay();
+        console.log(i);
+         console.log(thatDateofWeek);
+      
+    for (var counter=0; counter < lessonDays.length; counter++)  {
+if (thatDateofWeek == lessonDays[counter]) {
+    console.log(thatDateofWeek);
+    console.log(lessonDays);
+    // cDay[i].setAttribute('class', 'calenderDay');
+  selectedDays.push(i);
+    console.log(selectedDays);
+     var cDaySelected = document.getElementById(i);
+   cDaySelected.className += " selectedDay";
+   
+   var plans = document.createTextNode(currentLesson[counter]);
+   cDaySelected.appendChild(plans);
+ }
+ } } }
 
 
 findMonday();
 createCalender(daysInCurrentMonth);
 
+getSelectedDays(lessonDays);
 
 } (window));
 
